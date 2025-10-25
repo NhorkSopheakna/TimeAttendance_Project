51f48f76b1af2b1327701faa37f0472de2509194
NhorkSopheakna <99951792+NhorkSopheakna@users.noreply.github.com>
2025-10-26 04:18:51 +0700

    Add files via upload

---
+++ 01_create_collections.js
@@ -0,0 +1,9 @@
+use TimeAttendanceDB;
+db.createCollection("fingerprint_scans");
+db.createCollection("device_logs");
+db.createCollection("sync_status");
+db.createCollection("anomalies");
+db.createCollection("gps_events");
+
+
+
